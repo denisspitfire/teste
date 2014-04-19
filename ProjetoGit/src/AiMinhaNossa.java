97f112f3feb2c93fce4e954e95f1614915ffd4ff
EngineSystem <enginesy@gmail.com>
2014-04-19 15:30:52 -0300

    mensagem do commit

---
+++ ProjetoGit/src/AiMinhaNossa.java
@@ -0,0 +1,11 @@
+public class AiMinhaNossa {
+	String mensagem;
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+}
---
+++ ProjetoGit/src/Teste2.java
@@ -0,0 +1,12 @@
+
+public class Teste2 {
+	String ae;
+
+	public String getAe() {
+		return ae;
+	}
+
+	public void setAe(String ae) {
+		this.ae = ae;
+	}
+}
